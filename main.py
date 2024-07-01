27f80dc909ab3512627de8c498b8afc417faa86b
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-07-01 14:06:18 +0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
